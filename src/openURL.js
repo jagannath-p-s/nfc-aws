909d1f542facdcc5c27acc4a56f5708a7df17174
jagannath-p-s <jagannathps2000@gmail.com>
2024-04-29 17:55:31 +0530

    heeeeeee

--- src/openURL.js
+++ src/openURL.js
@@ -1,11 +1,13 @@
 export function openURL(url) {
-    if (url.startsWith('upi://pay')) {
-        window.location.href = url;
-    } else if (url.startsWith('https://g.co/kgs')) {
-        window.location.href = url;
-    } else if (url.startsWith('mailto:')) {
-        window.location.href = url;
+    if (url.startsWith('mailto:') || url.startsWith('tel:')) {
+      const anchor = document.createElement('a');
+      anchor.href = url;
+      anchor.target = '_blank'; // Ensures opening in a new tab or app
+      anchor.rel = 'noopener noreferrer'; // Security precaution
+      anchor.click(); // Programmatically triggers the click
+    } else if (url.startsWith('upi://')) {
+      window.location.href = url; // Opens UPI links
     } else {
-        window.open(url, '_blank');
+      window.open(url, '_blank'); // Opens other URLs in a new tab
     }
-}
+  }
