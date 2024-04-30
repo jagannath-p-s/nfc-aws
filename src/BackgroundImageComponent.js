4d47e6ac1a0f86e3f6de642006fd1e58bd548f03
jagannath p s <110271239+jagannath-p-s@users.noreply.github.com>
2024-04-30 11:59:13 +0530

    fixed BackgroundImageComponent.js

--- src/BackgroundImageComponent.js
+++ src/BackgroundImageComponent.js
@@ -7,23 +7,14 @@ const BackgroundImage = styled.div`
     top: 0;
     left: 0;
     opacity: 0.3;
-    width: 100vw; /* Use viewport width */
-    height: 100vh; /* Use viewport height */
+    width: 130vw; /* Use viewport width */
+    height: 130vh; /* Use viewport height */
     z-index: -1;
     background-image: url(${props => props.src});
-    background-size: cover;
+    background-size: 130% 130%; /* Stretch the background image to cover the entire page */
     background-position: center;
-    /* Add overlay */
-    &:after {
-        content: "";
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        background-color: rgba(0, 0, 0, 0.3); /* Black overlay color with opacity */
-    }
-`;
+   
+`;;
 
 // BackgroundImageComponent to render the background image
 const BackgroundImageComponent = ({ src }) => (
